c7d1d4babe9055410296da85857a0cf8ec39c992
Tomas Jansson <tomas.jansson@gmail.com>
2013-05-12 16:16:05 +0200

    added deploy scripts

---
+++ DemoProject.Web/DeployScripts/DoConfig.ps1
@@ -0,0 +1,17 @@
+﻿$ErrorActionPreference = "Stop"
+
+function Get-ScriptDirectory
+{
+    Split-Path $script:MyInvocation.MyCommand.Path
+}
+
+$configFile = "Dev.Config.ps1"
+$configFilePath = (Get-ScriptDirectory) + "\$configFile"
+
+$IISConfigurationScriptPath = (Get-ScriptDirectory) + "\IISConfiguration.ps1"
+. $IISConfigurationScriptPath
+
+. $configFilePath
+
+CreateAppPools $config.ApplicationPools
+CreateSiteFromConfig $config.Site
