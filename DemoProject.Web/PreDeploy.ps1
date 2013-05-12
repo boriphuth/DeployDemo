6c8570c968b10582cf73ed155b1c07e86d625477
Tomas Jansson <tomas.jansson@gmail.com>
2013-05-12 17:21:21 +0200

    fixed octopuswebsitename

---
+++ DemoProject.Web/PreDeploy.ps1
@@ -0,0 +1,7 @@
+﻿function Get-ScriptDirectory
+{
+    Split-Path $script:MyInvocation.MyCommand.Path
+}
+
+$doConfigPath = (Get-ScriptDirectory) + "\DeployScripts\" + "DoConfig.ps1"
+. $doConfigPath
