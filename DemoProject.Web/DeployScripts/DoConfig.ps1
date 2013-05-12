f987fe47316bb66a8d7ae6bf7581fddc573b8691
Tomas Jansson <tomas.jansson@gmail.com>
2013-05-12 17:20:58 +0200

    fixed conflict

--- DemoProject.Web/DeployScripts/DoConfig.ps1
+++ DemoProject.Web/DeployScripts/DoConfig.ps1
@@ -5,7 +5,21 @@ function Get-ScriptDirectory
     Split-Path $script:MyInvocation.MyCommand.Path
 }
 
-$configFile = "Dev.Config.ps1"
+function Get-OctopusWebSiteNameFromConfig($conf) {
+    if($conf.Site) {
+        if($conf.Site.Application) {
+            return $conf.Site.Name + "/" + $conf.Site.Application.Name
+        }
+        return $conf.Site.Name
+    }
+    Write-Error "Configuration is missing site"
+    exit 1
+}
+
+if($configFile -eq $null) {
+    $configFile = "Local.Config.ps1"
+}
+
 $configFilePath = (Get-ScriptDirectory) + "\$configFile"
 
 $IISConfigurationScriptPath = (Get-ScriptDirectory) + "\IISConfiguration.ps1"
@@ -15,3 +29,5 @@ $IISConfigurationScriptPath = (Get-ScriptDirectory) + "\IISConfiguration.ps1"
 
 CreateAppPools $config.ApplicationPools
 CreateSiteFromConfig $config.Site
+
+Set-OctopusVariable -Name "OctopusWebSiteName" -Value (Get-OctopusWebSiteNameFromConfig $config)
